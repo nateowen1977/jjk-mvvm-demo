ed37511a9a9635e3018d00a645c9e7bbc0ec24eb
Owen, Nate <nowen@jjkeller.com>
2015-08-15 22:51:34 -0500

    everything working except binding of the age component to/from the edit-contact component.

--- client/contact-edit.js
+++ client/contact-edit.js
@@ -2,13 +2,21 @@ import {bindable} from 'aurelia-framework';
 
 export class ContactEdit{
     title = "contact edit";
+    contact = {
+        first: null,
+        last: null,
+        age: null
+    };
 
     save(){
-        this.parent.addContact({
-            first: "corey",
-            last: "arts",
-            age: 37
-        });
+        this.contact.age = 38;
+        if(this.contact.first && this.contact.last && this.contact.first.length > 0 && this.contact.last.length > 0 && this.contact.age){
+            this.parent.addContact(this.contact);
+        }
+    }
+
+    clear(){
+        this.contact = null;
     }
 
     bind(bindingContext){
