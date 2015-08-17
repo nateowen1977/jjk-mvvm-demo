3995100ca4b38e67fefb67a2dbb57b59f9f5ef3f
Owen, Nate <nowen@jjkeller.com>
2015-08-17 12:53:16 -0500

    finished services demo

--- client/controller.js
+++ client/controller.js
@@ -7,24 +7,6 @@
                 ContactList : null
             };
 
-            $scope.saveContact = function(contact){
-                contactService.Save(contact.FirstName, contact.LastName, contact.Age)
-                    .then(function(){
-                        contactService.Get().then(function(data){
-                            $scope.Model.ContactList = data;
-                        });
-                    })
-            };
-
-            $scope.deleteContact = function(contact){
-                contactService.Delete(contact.Id)
-                    .then(function(){
-                        contactService.Get().then(function(data){
-                            $scope.Model.ContactList = data;
-                        });
-                    });
-            }
-
             contactService.Get().then(function(data){
                 $scope.Model.ContactList = data;
             })
--- client/service.js
+++ client/service.js
@@ -3,45 +3,15 @@
         .module('contactListApp')
         .service('contactService', function ($http, $q) {
             return {
-                Get: getContact,
-                Save: saveContact,
-                Delete: deleteContact
+                Get: getContacts,
             }
 
-            function getContact() {
+            function getContacts() {
                 var deferred = $q.defer();
                 $http.get('/api/contacts')
                     .then(function (response) {
                         deferred.resolve(response.data);
                     })
-
-                return deferred.promise;
-            }
-
-            function saveContact(first, last, age) {
-                var deferred = $q.defer();
-
-                var payload = {
-                    first: first,
-                    last: last,
-                    age: age
-                };
-
-                $http.post('/api/contacts/', payload)
-                    .then(function () {
-                        deferred.resolve();
-                    });
-
-                return deferred.promise;
-            }
-
-            function deleteContact(id){
-                var deferred = $q.defer();
-
-                $http.delete('/api/contacts/' + id)
-                    .then(function () {
-                        deferred.resolve();
-                    });
 
                 return deferred.promise;
             }
